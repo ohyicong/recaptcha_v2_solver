ff91435d89dbabc599659e0c99174cd529e3563a
Dmitry Agafonov <malkiz@yandex.ru>
2021-06-08 01:31:34 +0300

    Add PEP8 support

--- recaptcha_solver.py
+++ recaptcha_solver.py
@@ -32,18 +32,19 @@ import pydub
 #custom patch libraries
 import patch 
 
-def delay ():
-    time.sleep(random.randint(2,3))
+
+def delay():
+    time.sleep(random.randint(2, 3))
 
 
-if __name__ =="__main__":
-    #download latest chromedriver, please ensure that your chrome is up to date
-    while(True):
+if __name__ == "__main__":
+    # download latest chromedriver, please ensure that your chrome is up to date
+    while True:
         try:
-            #create chrome driver
-            driver = webdriver.Chrome(os.path.normpath(os.getcwd()+"\\webdriver\\chromedriver.exe")) 
+            # create chrome driver
+            driver = webdriver.Chrome(os.path.normpath(os.getcwd() + "\\webdriver\\chromedriver.exe"))
             delay()
-            #go to website
+            # go to website
             driver.get("https://www.google.com/recaptcha/api2/demo")
             break
         except:
@@ -57,58 +58,59 @@ if __name__ =="__main__":
             if (not is_patched): 
                 print("[-] Please update the chromedriver.exe in the webdriver folder according to your chrome version:https://chromedriver.chromium.org/downloads")
                 break
-            
-    #main program        
-    #switch to recaptcha frame
-    frames=driver.find_elements_by_tag_name("iframe")
-    driver.switch_to.frame(frames[0]);
-    delay()
-    
-    #click on checkbox to activate recaptcha
-    driver.find_element_by_class_name("recaptcha-checkbox-border").click()
-    
-    #switch to recaptcha audio control frame
-    driver.switch_to.default_content()
-    frames=driver.find_element_by_xpath("/html/body/div[2]/div[4]").find_elements_by_tag_name("iframe")
+
+    # main program
+    # switch to recaptcha frame
+    frames = driver.find_elements_by_tag_name("iframe")
     driver.switch_to.frame(frames[0])
     delay()
-    
-    #click on audio challenge
-    driver.find_element_by_id("recaptcha-audio-button").click()
-    
-    #switch to recaptcha audio challenge frame
+
+    # click on checkbox to activate recaptcha
+    driver.find_element_by_class_name("recaptcha-checkbox-border").click()
+
+    # switch to recaptcha audio control frame
     driver.switch_to.default_content()
-    frames= driver.find_elements_by_tag_name("iframe")
+    frames = driver.find_element_by_xpath("/html/body/div[2]/div[4]").find_elements_by_tag_name("iframe")
+    driver.switch_to.frame(frames[0])
+    delay()
+
+    # click on audio challenge
+    driver.find_element_by_id("recaptcha-audio-button").click()
+
+    # switch to recaptcha audio challenge frame
+    driver.switch_to.default_content()
+    frames = driver.find_elements_by_tag_name("iframe")
     driver.switch_to.frame(frames[-1])
     delay()
-    
-    #click on the play button
+
+    # click on the play button
     driver.find_element_by_xpath("/html/body/div/div/div[3]/div/button").click()
-    
-    #get the mp3 audio file
+
+    # get the mp3 audio file
     src = driver.find_element_by_id("audio-source").get_attribute("src")
-    print("[INFO] Audio src: %s"%src)
-    
-    #download the mp3 audio file from the source
-    urllib.request.urlretrieve(src, os.path.normpath(os.getcwd()+"\\sample.mp3"))
+    print("[INFO] Audio src: %s" % src)
+
+    # download the mp3 audio file from the source
+    urllib.request.urlretrieve(src, os.path.normpath(os.getcwd() + "\\sample.mp3"))
     delay()
-    
-    #load downloaded mp3 audio file as .wav
+
+    # load downloaded mp3 audio file as .wav
     try:
-        sound = pydub.AudioSegment.from_mp3(os.path.normpath(os.getcwd()+"\\sample.mp3"))
-        sound.export(os.path.normpath(os.getcwd()+"\\sample.wav"), format="wav")
-        sample_audio = sr.AudioFile(os.path.normpath(os.getcwd()+"\\sample.wav"))
-    except:
-        print("[-] Please run program as administrator or download ffmpeg manually, http://blog.gregzaal.com/how-to-install-ffmpeg-on-windows/")
-        
-    #translate audio to text with google voice recognition
-    r= sr.Recognizer()
+        sound = pydub.AudioSegment.from_mp3(os.path.normpath(os.getcwd() + "\\sample.mp3"))
+        sound.export(os.path.normpath(os.getcwd() + "\\sample.wav"), format="wav")
+        sample_audio = sr.AudioFile(os.path.normpath(os.getcwd() + "\\sample.wav"))
+    except Exception:
+        print("[-] Please run program as administrator or download ffmpeg manually, "
+              "http://blog.gregzaal.com/how-to-install-ffmpeg-on-windows/")
+
+    # translate audio to text with google voice recognition
+    r = sr.Recognizer()
     with sample_audio as source:
         audio = r.record(source)
-    key=r.recognize_google(audio)
-    print("[INFO] Recaptcha Passcode: %s"%key)
-    
-    #key in results and submit
+    key = r.recognize_google(audio)
+    print("[INFO] Recaptcha Passcode: %s" % key)
+
+    # key in results and submit
     driver.find_element_by_id("audio-response").send_keys(key.lower())
     driver.find_element_by_id("audio-response").send_keys(Keys.ENTER)
     driver.switch_to.default_content()
--- recaptcha_solver_dynamic_ip_mac.py
+++ recaptcha_solver_dynamic_ip_mac.py
@@ -5,19 +5,19 @@ Created on Sun Aug 16 10:01:10 2020
 @author: OHyic
 """
 
-#system libraries
+# system libraries
 import subprocess
 import random
 import os
 import random
 import time
 import sys
-#selenium libraries
+# selenium libraries
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException   
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -25,15 +25,14 @@ from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import UnexpectedAlertPresentException
 from selenium.webdriver.chrome.options import Options
 
-
-#recaptcha libraries
+# recaptcha libraries
 import speech_recognition as sr
 import ffmpy
 import requests
 import urllib
 import pydub
 
-#Networking Libraries
+# Networking Libraries
 from spoofmac.util import random_mac_address, MAC_ADDRESS_R, normalize_mac_address
 from spoofmac.interface import (
     wireless_port_names,
@@ -45,19 +44,24 @@ from spoofmac.interface import (
 import socket
 import netifaces
 
-def delay (low=3,high=5):
-    time.sleep(random.randint(low,high))
 
-def change_ip(interface_name,ip_address,mask,gateway):
-    ip_address='.'.join(ip_address.split('.')[:-1])+'.'+ str(random.randrange(8,255-int(mask.split('.')[-1])-1))
-    result_1=subprocess.call('netsh interface ipv4 set address name="%s" static %s %s %s'%(interface_name,ip_address,mask,gateway), shell=True)
-    result_2=subprocess.call('netsh interface ipv4 set dns name="%s" static 8.8.8.8'%(interface_name), shell=True)
-    if(result_1==1 or result_2==1):
+def delay(low=3, high=5):
+    time.sleep(random.randint(low, high))
+
+
+def change_ip(interface_name, ip_address, mask, gateway):
+    ip_address = '.'.join(ip_address.split('.')[:-1]) + '.' + str(
+        random.randrange(8, 255 - int(mask.split('.')[-1]) - 1))
+    result_1 = subprocess.call(
+        'netsh interface ipv4 set address name="%s" static %s %s %s' % (interface_name, ip_address, mask, gateway),
+        shell=True)
+    result_2 = subprocess.call('netsh interface ipv4 set dns name="%s" static 8.8.8.8' % (interface_name), shell=True)
+    if result_1 == 1 or result_2 == 1:
         print("[WARN] Unable to change IP. Run the program with admin rights.")
         sys.exit()
-        return False
-    print("[INFO] New IP Address is: %s"%(ip_address))
+    print("[INFO] New IP Address is: %s" % ip_address)
     return True
+
 
 def get_default_network_details():
     def get_ip_address():
@@ -66,87 +70,92 @@ def get_default_network_details():
         ip_address=s.getsockname()[0]
         s.close()
         return ip_address
+
     ip_address = get_ip_address()
     for i in netifaces.interfaces():
         try:
-            if(str(netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr'])==str(ip_address)):
+            if str(netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr']) == str(ip_address):
                 print("[INFO] *Default Network Details*")
                 print("[INFO] IP Address: ", ip_address)
                 print("[INFO] Mask: ", netifaces.ifaddresses(i)[netifaces.AF_INET][0]['netmask'])
                 print("[INFO] Gateway: ", netifaces.gateways()['default'][netifaces.AF_INET][0])
-                delay(5,5)
-                return ip_address,netifaces.ifaddresses(i)[netifaces.AF_INET][0]['netmask'],netifaces.gateways()['default'][netifaces.AF_INET][0]
-        except:pass
+                delay(5, 5)
+                return ip_address, \
+                       netifaces.ifaddresses(i)[netifaces.AF_INET][0]['netmask'], \
+                       netifaces.gateways()['default'][netifaces.AF_INET][0]
+        except Exception:
+            pass
 
-#SETTINGS
-#Please check your network details 
+
+# SETTINGS
+# Please check your network details
 interface_name = 'Wi-Fi'
-ip_address,mask,gateway = get_default_network_details()
-count_loops=0
-count_failures=0
+ip_address, mask, gateway = get_default_network_details()
+count_loops = 0
+count_failures = 0
 
-#endless loop
-while(True):
+# endless loop
+while True:
     try:
         print("[INFO] *Changing MAC & IP*")
-        print("[INFO] New Mac is: "+random_mac_address(interface_name))
-        delay(3,5)
-        change_ip(interface_name,ip_address,mask,gateway)
-        delay(3,5)
+        print("[INFO] New Mac is: " + random_mac_address(interface_name))
+        delay(3, 5)
+        change_ip(interface_name, ip_address, mask, gateway)
+        delay(3, 5)
         print("[INFO] Start Browser")
-        driver = webdriver.Chrome(os.getcwd()+"\\webdriver\\chromedriver.exe") 
-        for i in range(10):   
-            count_loops+=1
-            #go to website
+        driver = webdriver.Chrome(os.getcwd() + "\\webdriver\\chromedriver.exe")
+        for i in range(10):
+            count_loops += 1
+            # go to website
             driver.get("https://www.google.com/recaptcha/api2/demo")
-            element=WebDriverWait(driver, 10).until(
+            element = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.TAG_NAME, "iframe"))
             )
-            #switch to recaptcha frame
-            frames=driver.find_elements_by_tag_name("iframe")
-            driver.switch_to.frame(frames[0]);
-            delay()
-            
-            #click on checkbox to activate recaptcha
-            driver.find_element_by_class_name("recaptcha-checkbox-border").click()
-            
-            #switch to recaptcha audio control frame
-            driver.switch_to.default_content()
-            frames=driver.find_element_by_xpath("/html/body/div[2]/div[4]").find_elements_by_tag_name("iframe")
+            # switch to recaptcha frame
+            frames = driver.find_elements_by_tag_name("iframe")
             driver.switch_to.frame(frames[0])
             delay()
-            
-            #click on audio challenge
-            driver.find_element_by_id("recaptcha-audio-button").click()
-            
-            #switch to recaptcha audio challenge frame
+
+            # click on checkbox to activate recaptcha
+            driver.find_element_by_class_name("recaptcha-checkbox-border").click()
+
+            # switch to recaptcha audio control frame
             driver.switch_to.default_content()
-            frames= driver.find_elements_by_tag_name("iframe")
+            frames = driver.find_element_by_xpath("/html/body/div[2]/div[4]").find_elements_by_tag_name("iframe")
+            driver.switch_to.frame(frames[0])
+            delay()
+
+            # click on audio challenge
+            driver.find_element_by_id("recaptcha-audio-button").click()
+
+            # switch to recaptcha audio challenge frame
+            driver.switch_to.default_content()
+            frames = driver.find_elements_by_tag_name("iframe")
             driver.switch_to.frame(frames[-1])
             delay()
-            #click on the play button
+            # click on the play button
             driver.find_element_by_xpath("/html/body/div/div/div[3]/div/button").click()
-            #get the mp3 audio file
+            # get the mp3 audio file
             src = driver.find_element_by_id("audio-source").get_attribute("src")
-            print("[INFO] Audio src: %s"%src)
-            #download the mp3 audio file from the source
-            urllib.request.urlretrieve(src, os.getcwd()+"\\sample.mp3")
-            sound = pydub.AudioSegment.from_mp3(os.getcwd()+"\\sample.mp3")
-            sound.export(os.getcwd()+"\\sample.wav", format="wav")
-            sample_audio = sr.AudioFile(os.getcwd()+"\\sample.wav")
-            r= sr.Recognizer()
-            
+            print("[INFO] Audio src: %s" % src)
+            # download the mp3 audio file from the source
+            urllib.request.urlretrieve(src, os.getcwd() + "\\sample.mp3")
+            sound = pydub.AudioSegment.from_mp3(os.getcwd() + "\\sample.mp3")
+            sound.export(os.getcwd() + "\\sample.wav", format="wav")
+            sample_audio = sr.AudioFile(os.getcwd() + "\\sample.wav")
+            r = sr.Recognizer()
+
             with sample_audio as source:
                 audio = r.record(source)
-            
-            #translate audio to text with google voice recognition
-            key=r.recognize_google(audio)
-            print("[INFO] Recaptcha Passcode: %s"%key)
-            #key in results and submit
+
+            # translate audio to text with google voice recognition
+            key = r.recognize_google(audio)
+            print("[INFO] Recaptcha Passcode: %s" % key)
+            # key in results and submit
             delay()
             for letter in key.lower().split():
                 driver.find_element_by_id("audio-response").send_keys(letter)
-                delay(1,3)
+                delay(1, 3)
             delay()
             driver.find_element_by_id("audio-response").send_keys(Keys.ENTER)
             delay()
@@ -155,16 +164,15 @@ while(True):
             delay()
             driver.find_element_by_id("recaptcha-demo-submit").click()
             delay()
-            print("[INFO] Loop Completed (%d). Total Loops: %d, Total Failures %d"%(i+1,count_loops,count_failures))
+            print(
+                "[INFO] Loop Completed (%d). Total Loops: %d, Total Failures %d" % (i + 1, count_loops, count_failures))
             print("[INFO] Waiting for 10 seconds.. before trying again ")
-            delay(10,10)
+            delay(10, 10)
         driver.close()
-        driver.quit()    
-    except:
-        count_failures+=1
-        print("[WARN] Something went wrong. Total Loops: %d, Total Failures %d"%(count_loops,count_failures))
+        driver.quit()
+    except Exception:
+        count_failures += 1
+        print("[WARN] Something went wrong. Total Loops: %d, Total Failures %d" % (count_loops, count_failures))
         print("[INFO] Reopening Driver")
         driver.close()
         driver.quit()
-
-       
