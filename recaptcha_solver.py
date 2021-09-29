10a51dd7bbf4e0e49c92666611d60f132d1862df
Alessio DS <digialessio63@gmail.com>
2021-09-29 22:26:54 +0200

    Uncompromising formatting.

--- recaptcha_solver.py
+++ recaptcha_solver.py
@@ -13,6 +13,7 @@ import urllib
 # recaptcha libraries
 import pydub
 import speech_recognition as sr
+
 # selenium libraries
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -20,8 +21,10 @@ from selenium.webdriver.common.keys import Keys
 # custom patch libraries
 from patch import download_latest_chromedriver, webdriver_folder_name
 
+
 def delay(waiting_time=5):
     driver.implicitly_wait(waiting_time)
+
 
 if __name__ == "__main__":
     # download latest chromedriver, please ensure that your chrome is up to date
@@ -29,7 +32,8 @@ if __name__ == "__main__":
         try:
             # create chrome driver
             path_to_chromedriver = os.path.normpath(
-                os.path.join(os.getcwd(), webdriver_folder_name, 'chromedriver.exe'))
+                os.path.join(os.getcwd(), webdriver_folder_name, "chromedriver.exe")
+            )
             driver = webdriver.Chrome(path_to_chromedriver)
             delay()
             # go to website
@@ -42,22 +46,25 @@ if __name__ == "__main__":
             except NameError:
                 is_patched = download_latest_chromedriver()
             else:
-                is_patched = download_latest_chromedriver(driver.capabilities['version'])
+                is_patched = download_latest_chromedriver(
+                    driver.capabilities["version"]
+                )
             if not is_patched:
                 sys.exit(
                     "[ERR] Please update the chromedriver.exe in the webdriver folder according to your chrome version:"
-                    "https://chromedriver.chromium.org/downloads")
+                    "https://chromedriver.chromium.org/downloads"
+                )
     # main program
     # auto locate recaptcha frames
     frames = driver.find_elements_by_tag_name("iframe")
     recaptcha_control_frame = None
     recaptcha_challenge_frame = None
-    for index,frame in enumerate(frames):
-        if (frame.get_attribute("title")=="reCAPTCHA"):
+    for index, frame in enumerate(frames):
+        if frame.get_attribute("title") == "reCAPTCHA":
             recaptcha_control_frame = frame
-        if (frame.get_attribute("title")=="recaptcha challenge"):
+        if frame.get_attribute("title") == "recaptcha challenge":
             recaptcha_challenge_frame = frame
-    if (not (recaptcha_control_frame and recaptcha_challenge_frame)):
+    if not (recaptcha_control_frame and recaptcha_challenge_frame):
         print("[ERR] Unable to find recaptcha. Abort solver.")
         exit()
     # switch to recaptcha frame
@@ -96,8 +103,10 @@ if __name__ == "__main__":
         sound.export(path_to_wav, format="wav")
         sample_audio = sr.AudioFile(path_to_wav)
     except Exception:
-        sys.exit("[ERR] Please run program as administrator or download ffmpeg manually, "
-                 "https://blog.gregzaal.com/how-to-install-ffmpeg-on-windows/")
+        sys.exit(
+            "[ERR] Please run program as administrator or download ffmpeg manually, "
+            "https://blog.gregzaal.com/how-to-install-ffmpeg-on-windows/"
+        )
 
     # translate audio to text with google voice recognition
     r = sr.Recognizer()
